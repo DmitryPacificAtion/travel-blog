99254972e5d90f5a1f52b0fa2688419007f80a60
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2020-06-01 18:10:14 +0300

    Finished mobile footer

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 (function () {
+  /* Dropdown handlers */
   const menuItems = Object.values(document.querySelectorAll('.menu__item'));
   const submenuItems = Object.values(document.querySelectorAll('.submenu__item'));
 
@@ -18,4 +19,21 @@
 
   menuItems.forEach((list) => list.addEventListener('click', menuItemHandler));
   submenuItems.forEach((list) => list.addEventListener('click', submenuItemHandler));
+
+
+
+  /* Mobile menu handlers */
+  const burger = document.getElementById('burger');
+  const mobSide = document.getElementById('mobile-sidemenu');
+  const mobSideClose = document.getElementById('mobile-sidemenu-close');
+  const mobSideHandler = () => {
+    const isActive = mobSide.classList.contains('active');
+    mobSide.classList.toggle('active', !isActive);
+  }
+  if(burger !== null) {
+    burger.addEventListener('touchend', mobSideHandler);
+    burger.addEventListener('click', mobSideHandler);
+    mobSideClose.addEventListener('touchend', mobSideHandler);
+    mobSideClose.addEventListener('click', mobSideHandler);
+  }
 })();
